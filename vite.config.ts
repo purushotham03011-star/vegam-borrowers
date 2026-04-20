ba80077cc7e9fa1c85033fabb7d99adfa92db96f
purushotham03011-star <purushotham.03011@gmail.com>
2026-04-20 13:42:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react(), tailwindcss()],
       define: {
-        'process.env': env,
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
