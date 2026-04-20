eea4977f06936c4f6b307f2f94166cc55940754d
purushotham03011-star <purushotham.03011@gmail.com>
2026-04-20 14:28:54 +0530

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-
-// This service has been deprecated. 
-// Please use services/loanService.ts with Supabase.
-export const saveToGoogleSheet = async (data: any) => {
-  console.warn("Google Sheets integration is removed. Use Supabase.");
-  return true; 
-};
